64298c10a8dff194e8100e074eb0808c3810b797
Cameron Raissi <cameronraissi@Camerons-MacBook-Pro.local>
2019-11-02 01:45:07 -0400

    In an effort to show more of whatever it is I find myself working on, here's what I've been fiddling with sequencer. Hit the space bar to play. Definitely nothing of immediate note, just some sort of simple proof of concept on how p5js can be easier to use and implement I guess. I'm going to get the rest of the sequencer (animation+user input, stop/start button, etc.) going later this weekend

---
+++ src/app/sequencer/sequencer.component.ts
@@ -0,0 +1,126 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as p5 from 'p5';
+import "p5/lib/addons/p5.sound";
+import "p5/lib/addons/p5.dom";
+
+/*
+//hh, clap, bass, audiofiles & loop callbacks
+let hh, clap, bass;
+//PATTERNS. Array of numbers to manipulate when instrument get called
+let hPat, cPat, bPat;
+//PHRASES. Defines how PATTERNS get interpreted
+let hPhrase, cPhrase, bPhrase;
+//DRUMRACK. Attach PHRASES to DRUMRACK's PARTS. Basically sequence transport
+let drums;
+//bpm
+let bpmCTRL;
+//number of beat steps
+let beatLength;
+//variable for canvas
+let cnv;
+//The steps of the sequencer
+let sPat;
+//position of highlighted column when sequencer is playing
+let cursorPos;
+*/
+
+@Component({
+  selector: 'app-sequencer',
+  templateUrl: './sequencer.component.html',
+  styleUrls: ['./sequencer.component.scss']
+})
+export class SequencerComponent implements OnInit {
+
+  private p5;
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+    const sketch = (s) => {
+
+      s.preload = () => {
+        let hh, clap, bass;
+        let hPat, cPat, bPat;
+        let hPhrase, cPhrase, bPhrase;
+        let drums; //Basically the drumrack. attach phrases to drumrack's parts. ~sequencer transport
+        let bpmCTRL; //bpm
+        let beatLength; //number of beat steps
+      }
+
+      s.setup = () => {
+        s.createCanvas(800, 150);
+        //assign instance of # of steps & the visual intervals of the sequencer cells
+        s.beatLength = 16;
+        s.cellWidth = s.width/s.beatLength;
+        //loading instruments w/ their respective audio files
+        s.hh = s.loadSound('assets/hh_sample.mp3', () => {} );
+        s.clap = s.loadSound('assets/clap_sample.mp3', () => {} );
+        s.bass = s.loadSound('assets/bass_sample.mp3', () => {} );
+        //hardcoding each instruments default patterns
+        s.hPat = [0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0];
+        s.cPat = [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1];
+        s.bPat = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0];
+        //each patterns respective phrase
+        s.hPhrase = new p5.Phrase('hh', (time) => {s.hh.play(time); console.log(time); }, s.hPat);
+        s.cPhrase = new p5.Phrase('clap', (time) => {s.clap.play(time); console.log(time); }, s.cPat);
+        s.bPhrase = new p5.Phrase('bass', (time) => {s.bass.play(time); console.log(time); }, s.bPat);
+        //initialize drums variable add each instruments' phrasing to collection of all instruments on drums
+        s.drums = new p5.Part();
+        s.drums.addPhrase(s.hPhrase);
+        s.drums.addPhrase(s.cPhrase);
+        s.drums.addPhrase(s.bPhrase);
+        //hardcode global bpm, create slider to control bpm, position&enlarge it, assign slider value as bpm 
+        s.drums.setBPM(90);
+        s.bpmCTRL = s.createSlider(30, 200, 80, .5); //createSlider(min, max, initValue, incrementIntervals)
+        s.bpmCTRL.position(10, 175); s.bpmCTRL.style('width', '320px'); //positioning&enlarging slider
+        s.bpmCTRL.input( () => {s.drums.setBPM(s.bpmCTRL.value())} ); 
+        //sets canvas background color, sequencer line color&thickness; 
+        s.background(144,0,255); 
+        s.stroke('gray'); s.strokeWeight(2);
+        //logic for drawing the sequencer grid
+        for (let i = 0; i < s.beatLength; i++) { //line(startx,starty,endx,endy)
+          s.line(i*s.cellWidth,0,i*s.cellWidth,s.height); //basically just draws the columns
+        }
+        //logic for drawing rows
+        for (let i = 0; i < 4; i++) {
+          s.line(0, i*s.height/3, s.width, i*s.height/3);
+        }
+        //s.noStroke(); //disables
+        //assign cells with hardcoded Pat array values
+        for (let i=0; i < s.beatLength; i++) {
+          if(s.hPat[i] === 1){
+            s.ellipse(i*s.cellWidth +0.5*s.cellWidth, s.height/6, 30);
+          }
+          if (s.cPat[i] === 1) {
+            s.ellipse(i*s.cellWidth +0.5*s.cellWidth, s.height/2, 30);
+          }
+          if (s.bPat[i] === 1) {
+            s.ellipse(i*s.cellWidth +0.5*s.cellWidth, s.height*5/6, 30);
+          }
+        }
+
+
+      }
+
+      s.keyPressed = () => {
+        if (s.key === " ") {
+          if (s.hh.isLoaded() && s.clap.isLoaded() && s.bass.isLoaded()) {
+            if (!s.drums.isPlaying) {
+              s.drums.loop();
+            } else {
+              s.drums.stop();
+            }            
+          } else {
+            console.log('slowdown boi, audiofiles gotta load...');
+          }
+        }
+      }
+
+    }
+
+    let canvas = new p5(sketch);
+  }
+
+}
+
